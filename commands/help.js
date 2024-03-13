e19c83f50b94abaa983ed08d58836b39e71a7a62
topocai <leonjoserp@gmail.com>
2024-03-12 23:59:29 -0300

    Update variables.js import in help.js

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 
 const index = require('../index.js');
 
-const { LOCAL_COMMANDS_NAME, COMMANDS_CATEGORY, COMMAND_CATEGORY_DISPLAY_NAMES, COMMAND_CATEGORY_DESCRIPTIONS, GetKeyByValue } = require('../variables.js');
+const { LOCAL_COMMANDS_NAME, NORMAL_COMMANDS_NAME, COMMANDS_CATEGORY, COMMAND_CATEGORY_DISPLAY_NAMES, COMMAND_CATEGORY_DESCRIPTIONS, GetKeyByValue } = require('../variables.js');
 
 const config = require('../config.json');
 const commandName = config.LOCAL_MODE ? LOCAL_COMMANDS_NAME.HELP : NORMAL_COMMANDS_NAME.HELP;
